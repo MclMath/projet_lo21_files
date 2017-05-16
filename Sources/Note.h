d7a6c9876f25eb98db40f52c37b868d75ea3114c
Cedric Barreteau <cedric.barreteau@etu.utc.fr>
2017-05-16 13:32:18 +0200

    Merge avec le début de Michael

--- Sources/Note.h
+++ Sources/Note.h
@@ -1,23 +1,17 @@
-#include <string>
+#ifndef N
+#define N
+
+
+#include <timing.h>
+#include <QString>
+#include <QFile>
 
 enum Statut{attente,cours,terminee};
-enum Fichier{audio, video};
-
-class Date{
-  private:
-    int jour;
-    int mois;
-    int annee;
-  public:
-    int getJour(){return jour;};
-    int getMois(){return mois;};
-    int getAnnee(){return annee;};
-};
 
 class Note{
-  private:
-    std::string id;
-    std::string titre;
+  protected:
+    QString id;
+    QString titre;
     Date d_creat;
     Date d_modif;
 
@@ -26,39 +20,44 @@ class Note{
     // constructeurs et destructeurs ici pour que le Manager gère
 
   public:
-    std::string getTitre(){return titre;};
-    std::string getId(){return id;};
-    Date getCreat(){return d_creat;};
-    Date getModif(){return d_modif;};
+    Note(const QString& id,const QString& titre, const Date& date_cr, const Date& date_end):identifier(id),title(titre),date_creation(date_cr),date_last_modif(date_l){}
+    QString getTitre()const{return title;}
+    QString getId()const{return identifier;}
+    Date getCreat()const{return date_creation;}
+    Date getDModif()const{return date_last_modif;}
+    void setTitle(const QString& t);
 };
 
 class Tache : public Note{
   private:
-    std::string action;
+    QString action;
     int priorite;
     Date echeance;
     Statut statut;
   public:
-    std::string getAction(){return action;};
-    int getPriorite(){return priorite;};
-    Date getEcheance(){return echeance;};
-    Statut getStatut(){return statut;};
+    Tache(const QString& id,const QString& titre, const Date& date_cr, const Date& date_l, const QString& act, int prior, const Date& d_end, type_Status st):Note(id,titre,date_cr,date_l),action(act),priority(prior),date_echeance(d_end),status(st){};
+    QString getAction() const{return action;};
+    int getPriority() const{return priority;};
+    Date getDate_echeance() const{return date_echeance;};
+    QString getStatus() const{return status;};
 };
 
 class Article : public Note{
   private:
-    std::string texte;
+    QString texte;
   public:
-    std::string getTexte(){return texte;};
+    Article(const QString& id,const QString& titre, const Date& date_cr, const Date& date_l, const QString& txt):Note(id,titre,date_cr,date_l),text(txt){};
+    QString getTexte()const{return texte;};
+    void setText(const QString& txt);
 };
 
 class Multimedia : public Note{
   private:
-    std::string desc;
-    Fichier fichier;
+    QString desc;
+    QFile fichier;
   public:
-    std::string getDesc(){return desc;};
-    Fichier getFichier(){return fichier;};
+    QString getDesc(){return desc;};
+    QFile getFichier(){return fichier;};
 };
 
 class Relation{
@@ -67,6 +66,7 @@ class Relation{
     std::string description;
     Note* couple[2]; // Pour ça Note doit ne pas être abstraite
   public:
+
     //creation et tout le bazar
     // (très) probablement implémenter un iterator
 };
@@ -76,3 +76,5 @@ class Manager{
   private:
   public:
 };
+
+#endif
