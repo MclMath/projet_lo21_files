346020a6534a2ddadd19f5c71e5c1dfde9956a34
Michael Mahamat <michael.mahamat@etu.utc.fr>
2017-05-17 12:00:39 +0200

    Update connexion.h

--- Sources/connexion.h
+++ Sources/connexion.h
@@ -1,6 +1,7 @@
 #ifndef Connexion
 #define Connexion
-#include <sqlite3.h>
+//#include <sqlite3.h>
+#include "sqlite3.h" //semble marcher comme ça.
 
 sqlite3* connexion();
 void deconnexion(sqlite3* db);
